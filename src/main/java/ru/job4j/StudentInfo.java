52689b6256d648ebecc0b8e5e9fca1b0b397f5ce
alex <ganalex01@gmail.com>
2022-04-15 23:11:37 +0300

    исправления Checkstyle

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,11 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         System.out.println("Ganaga Alexey");
         System.out.println("21.04.1992");
-
-
-
     }
 }
