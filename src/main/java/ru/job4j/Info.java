14a6d1feb5e68063b23f9dbacf682646b6865a47
alex <ganalex01@gmail.com>
2022-04-15 22:52:00 +0300

    загрузка Info

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main (String[]args){
-        System.out.println("15.05.2022");
-
+    public static void main(String[] args) {
+        System.out.println("15.04.2022");
     }
- }
+}
