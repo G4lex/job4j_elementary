b92be4ca92c7b265ab2bd4cb4839eadd49f717ac
alex <ganalex01@gmail.com>
2022-04-15 22:21:58 +0300

    загрузка StudentInfo

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[]args){
+        System.out.println("Ganaga Alexey");
+        System.out.println("21.04.1992");
+
+
+
+    }
+}
