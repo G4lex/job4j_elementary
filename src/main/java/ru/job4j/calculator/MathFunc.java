3d1460b794f1edbaba51eb83f4dac4632fa372c7
alex <ganalex01@gmail.com>
2022-04-24 00:14:41 +0300

    MathFunc ver 1.2

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -4,10 +4,12 @@ public class MathFunc {
 
     public static int func1(int x) {
         int y = x * x + 1;
+        return y;
     }
 
     public static int func2(int x) {
         int y = 1 / x;
+        return y;
     }
 
     public static void main(String[] args) {
