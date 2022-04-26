da730d2114795a1d33a8bf18fc952e2d8f19b879
G4lex <ganalex01@gmail.com>
2022-04-27 00:59:13 +0300

    reload

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+       return value / 70;
+
+    }
+
+    public static float rubleToDollar(float value) {
+
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        int x = 100;
+        float euro = Converter.rubleToEuro(x);
+        System.out.println(x + " rubles are " + euro + " euro.");
+        float dollar = Converter.rubleToDollar(x);
+        System.out.println(x + " rubles are " + dollar + " dollar");
+        float euroS = euro / dollar;
+        String str = Float.toString(euroS);
+        System.out.println(dollar + " dollars are " + euroS + " euro");
+
+    }
+}
