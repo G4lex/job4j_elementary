87e5fa6b09260154455054ef73d4c8684d5ad5a0
G4lex <ganalex01@gmail.com>
2022-05-01 22:53:05 +0300

    Greeting ver 1/

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -15,6 +15,8 @@ public class MathFunc {
     public static void main(String[] args) {
        int result1 = MathFunc.func1(3);
        int result2 = MathFunc.func2(5);
+       int result3 = MathFunc.func1(100);
+       System.out.println(result3);
        int total = result1 + result2;
        System.out.println(total);
          }
