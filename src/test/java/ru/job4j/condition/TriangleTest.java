77481f3efd3109200339505fea3e4f1a5e9825ad
G4lex <ganalex01@gmail.com>
2022-05-08 00:12:32 +0300

    triangle ver 2

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
+
     @Test
     public void whenExist() {
         double ab = 2.0;
@@ -11,5 +12,14 @@ public class TriangleTest {
         double bc = 2.0;
         boolean result = Triangle.exist(ab, ac, bc);
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenNotExist() {
+        double ab = 0.0;
+        double ac = 2.0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertFalse(result);
     }
 }
