c10a15ad89fcc471d0c102ceec51373abb7430fe
G4lex <ganalex01@gmail.com>
2022-05-08 22:42:07 +0300

    max ver 2

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -9,7 +9,7 @@ public class MaxTest {
     public void whenMax2To1ThenLeft() {
         int left = 2;
         int right = 1;
-        int result = Max.left(left, right);
+        int result = Max.max(left, right);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
@@ -18,7 +18,7 @@ public class MaxTest {
     public void whenMax1To4ThenRight() {
         int left = 1;
         int right = 4;
-        int result = Max.left(right, left);
+        int result = Max.max(right, left);
         int expected = 4;
         Assert.assertEquals(result, expected);
     }
@@ -27,7 +27,7 @@ public class MaxTest {
     public void whenMax1To3ThenEqual() {
         int left = 3;
         int right = 3;
-        int result = Max.left(left, right);
+        int result = Max.max(left, right);
         int expected = 3;
         Assert.assertEquals(result, expected);
     }
