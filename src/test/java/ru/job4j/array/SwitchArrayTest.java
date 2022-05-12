9ddd9ff79a0492a19ca151c005ad0e6dfcd75072
G4lex <ganalex01@gmail.com>
2022-05-13 00:06:08 +0300

    swapTest ver 4

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -11,7 +11,7 @@ public class SwitchArrayTest {
     public void whenSwap0to3() {
         int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
         int source = 0;
-        int dest = input.length -1;
+        int dest = input.length - 1;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {8, 2, 3, 4, 5, 6, 7, 1};
         Assert.assertArrayEquals(expected, result);
