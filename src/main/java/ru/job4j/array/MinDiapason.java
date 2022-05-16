cf844d1b8c5e0214da8ffd69d235efa68651c20c
G4lex <ganalex01@gmail.com>
2022-05-16 22:36:20 +0300

    MinDiapason ver 3

--- src/main/java/ru/job4j/array/MinDiapason.java
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -1,11 +1,11 @@
 package ru.job4j.array;
 
 public class MinDiapason {
-    public static int findMin(int[] data, int start, int finish) {
-        int min = data[start];
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
             for (int i = start + 1; i <= finish; i++) {
-               if (data[i] < min) {
-                   min = data[i];
+               if (array[i] < min) {
+                   min = array[i];
                }
             }
         return min;
