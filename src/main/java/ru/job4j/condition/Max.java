305f30cde61e5ac3538c22a2d133e911a1118ac8
G4lex <ganalex01@gmail.com>
2022-06-02 00:28:40 +0300

    Перегруженные методы

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,12 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public int max(int left, int right, int mid) {
+        return max(left, max(right, mid));
+    }
+
+    public int max(int left, int right, int mid, int last) {
+        return max(left, max(right, max(mid, last)));
+    }
 }
